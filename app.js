7feaceda065db80f68ed08872b38d7445b5b6d19
feckmore <github@radiotree.com>
2014-09-02 22:25:36 -0400

    add indicators for turn and winner - closes #2, closes #5

--- app.js
+++ app.js
@@ -2,14 +2,20 @@
   var app = angular.module('factorWars', []);
 
   app.controller('FactorController', function(){
-    this.classes = {};
+    this.matrix = {};
     this.playerClass = ['info', 'danger'];
     this.score = [0, 0];
+    this.turns = [0, 0];
+    this.claimed = [0, 0];
     this.player = 0;
+    this.totalCells = 30;
 
     this.select = function(value){
       if(this.addScore(value, this.player)){
-        this.player = this.player === 0 ? 1: 0;
+        // keep track of the number of turns each player has made
+        this.turns[this.player]++;
+        // switch to the other player now
+        this.player = this.otherPlayer(this.player);
 
         var factors = this.getFactors(value);
         for(index = 0; index < factors.length; index++){
@@ -18,9 +24,14 @@
       }
     };
 
+    this.otherPlayer = function(player){
+      return player === 0 ? 1: 0;
+    };
+
     this.addScore = function(value, player){
       if(this.getClass(value) == undefined){
-        this.classes[value] = player;
+        this.claimed[player]++;
+        this.matrix[value] = player;
         this.score[player] = this.score[player] + value;
         return true;
       } else {
@@ -29,7 +40,7 @@
     };
 
     this.getClass = function(value){
-      return this.playerClass[this.classes[value]];
+      return this.playerClass[this.matrix[value]];
     };
 
     this.getFactors = function(value){
@@ -40,6 +51,22 @@
         }
       }
       return factors;
+    };
+
+    this.gameOver = function(){
+      var size = 0, key;
+      for (key in this.matrix) {
+        if (this.matrix.hasOwnProperty(key)) size++;
+      }
+      return size === this.totalCells;
+    };
+
+    this.showTurn = function(player){
+      return !this.gameOver() && this.player === player;
+    };
+
+    this.isWinner = function(player){
+      return this.gameOver() && this.score[player] > this.score[this.otherPlayer(player)];
     };
   });
 
